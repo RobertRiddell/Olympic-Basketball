778803fb8e2fb35c82e139d8a559cccf72b695f3
RobertRiddell <54061265+RobertRiddell@users.noreply.github.com>
2021-08-02 22:26:54 +1000

    Added fixtures for 2000 - 2016 Olympics

--- stats.py
+++ stats.py
@@ -5,20 +5,56 @@ import os
 import lxml.html as lh
 import pandas as pd
 
-URL = "https://www.basketball-reference.com/international/mens-olympics/2016-schedule.html#games"
-res = requests.get(URL)
+def olympic_fixtures(year):
+    URL = (f"https://www.basketball-reference.com/international/mens-olympics/{year}-schedule.html#games")
+    res = requests.get(URL)
+    
+    # code from https://towardsdatascience.com/web-scraping-html-tables-with-python-c9baba21059
+    doc = lh.fromstring(res.content)
+    tr_elements = doc.xpath('//tr')
 
-doc = lh.fromstring(res.content)
+    col=[]
+    i=0
 
-tr_elements = doc.xpath('//tr')
+    for t in tr_elements[0]:
+        i+=1
+        name=t.text_content()
+        col.append((name,[]))
 
-col=[]
-i=0
-#For each row, store each first element (header) and an empty list
-for t in tr_elements[0]:
-    i+=1
-    name=t.text_content()
-    print(i,name)
-    col.append((name,[]))
+    for j in range(1,len(tr_elements)):
+        #T is our j'th row
+        T=tr_elements[j]
+        
+        #If row is not of size 10, the //tr data is not from our table 
+        
+        if len(T)!=9:
+            break
+        
+        #i is the index of our column
+        i=0
+        
+        #Iterate through each element of the row
+        for t in T.iterchildren():
+            data=t.text_content() 
+            #Check if row is empty
+            if i>0:
+            #Convert any numerical value to integers
+                try:
+                    data=int(data)
+                except:
+                    pass
+            #Append the data to the empty list of the i'th column
+            col[i][1].append(data)
+            #Increment i for the next column
+            i+=1
 
-print(res.json)
+
+
+    Dict={title:column for (title,column) in col}
+    df=pd.DataFrame(Dict)
+    df.to_csv(f"data/fixtures/{year}-olympic-fixtures.csv")
+
+years = ['2000','2004','2008','2012','2016']
+
+for i in(years):
+    olympic_fixtures(i)
