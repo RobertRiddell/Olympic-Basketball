6de093cba79a07f06b24308f3366e303418473c2
RobertRiddell <54061265+RobertRiddell@users.noreply.github.com>
2021-08-04 21:13:43 +1000

    Stats.py works for individual pages.

--- stats.py
+++ stats.py
@@ -3,55 +3,38 @@ import datetime
 from dateutil.parser import parse
 from requests.api import request
 import requests
-import lxml.html as lh
+from bs4 import BeautifulSoup
+from bs4 import Comment
+
+# https://stackoverflow.com/questions/57032340/python-beautiful-soup-cant-find-specific-table
 
 dat = pd.read_csv("data/fixtures/2000-olympic-fixtures.csv")
-## https://www.basketball-reference.com/international/boxscores/2016-08-06-france.html
 dt = parse(dat['Date'][1])
+opp = dat['Opp'][1]
+opp = opp.lower()
+home_team = dat['Team'][1]
+home_team = home_team.lower()
 dt = datetime.datetime.strftime(dt, '%Y-%m-%d')
-URL = f'https://www.basketball-reference.com/international/boxscores/{dt}-france.html'
-res = requests.get(URL)
-doc = lh.fromstring(res.content)
-tr_elements = doc.xpath('//tr')
+URL = f'https://www.basketball-reference.com/international/boxscores/{dt}-{opp}.html'
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'html.parser')
+comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+tables = []
+for each in comments:
+    if 'table' in each:
+        try:
+            tables.append(pd.read_html(each)[0])
+        except:
+            continue
 
-col=[]
-i=0
+home = tables[0]
+away = tables[1]
 
-for t in tr_elements[0]:
-    i+=1 
-    name=t.text_content()
-    col.append((name,[]))
+home['Country'] = home_team
+away['Country'] = opp
+home['Date'] = dt
+away['Date'] = dt
+both = [home, away]
 
-for j in range(1,len(tr_elements)):
-    #T is our j'th row
-    T=tr_elements[j]
-        
-    #If row is not of size 10, the //tr data is not from our table 
-        
-    if len(T)!=9:
-        break
-        
-    #i is the index of our column
-    i=0
-        
-    #Iterate through each element of the row
-    for t in T.iterchildren():
-        data=t.text_content() 
-        #Check if row is empty
-        if i>0:
-        #Convert any numerical value to integers
-            try:
-                data=int(data)
-            except:
-                pass
-            #Append the data to the empty list of the i'th column
-        col[i][1].append(data)
-            #Increment i for the next column
-        i+=1
-
-
-
-Dict={title:column for (title,column) in col}
-df=pd.DataFrame(Dict)
-print(df)
-#df.to_csv(f"data/fixtures/{year}-olympic-fixtures.csv")
+complete = pd.concat(both)
+complete.to_csv(f'{dt}_{home_team}_{opp}.csv')
